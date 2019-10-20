a5688246451a406e06118155c1d102ac735d4adc
PradalCyril <pradal.cyril@gmail.com>
2019-10-20 17:20:52 +0200

    create home page and add react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route
+          exact
+          path='/'
+          render={() => (
+            <Redirect
+              to='/home'
+            />
+          )}
+        />
+        <Route path='/home' component={HomePage} />
+      </Switch>
     </div>
   );
 }
